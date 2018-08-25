6087eb69b8426d236905d19aa5c8612105adb6fe
contail <qkrtkdwls2121@gmail.com>
2018-08-25 13:44:09 +0900

    update urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
 
-from frontend.views import main_index, web_server
+from frontend.views import main_index, web_server,env_settings,change_env_values
 
 urlpatterns = [
     path('web_server/', web_server),
     path('main_index/', main_index),
+    path('env_settings/',env_settings),
+    path('change_env_values/',change_env_values)
 ]
