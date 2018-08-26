46b556f21752e67ce11204a77ea0534e2e5bf3a5
contail <qkrtkdwls2121@gmail.com>
2018-08-26 11:54:27 +0900

    add feedback url

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
 
-from frontend.views import main_index, web_server,env_settings,change_env_values
+from frontend.views import main_index, web_server,env_settings,change_env_values,feedback
 
 urlpatterns = [
     path('web_server/', web_server),
     path('main_index/', main_index),
     path('env_settings/',env_settings),
-    path('change_env_values/',change_env_values)
+    path('change_env_values/',change_env_values),
+    path('feedback/',feedback)
 ]
