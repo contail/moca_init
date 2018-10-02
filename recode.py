476439cca985dabd6a084a816d5a00091957845c
contail <qkrtkdwls2121@gmail.com>
2018-10-02 16:46:07 +0100

    change mp4

--- recode.py
+++ recode.py
@@ -1,9 +1,12 @@
+
 import picamera         #picamera를 불러온다.
 from time import sleep  #중간에 5초 딜레이를 위한 sleep 함수
 
 camera = picamera.PiCamera()         #picamera 생성
+camera.resolution = (640, 480)
+camera.framerate = 35
 camera.start_preview()
 camera.start_recording('test.h264') #녹화 시작
-sleep(5)                             #5초간 대기
+sleep(10)                             #5초간 대기
 camera.stop_recording()
 camera.stop_preview()
